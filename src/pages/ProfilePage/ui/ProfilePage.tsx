ff1cfe090504be8856d1dae7de29f423337f30db
Юрий <ydstolyaro@mts.ru>
2023-10-26 12:15:56 +0500

    feat: add profile page

--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -1,5 +1,17 @@
+import { profileReducer } from '@/entities/Profile';
+
+import { type ReducersList, DynamicModuleLoader } from '@/shared/lib/DynamicModuleLoader';
+
+const reducers: ReducersList = {
+  profile: profileReducer
+};
+
 const ProfilePage = () => {
-  return <div>1</div>;
+  return (
+    <DynamicModuleLoader reducers={reducers} isRemoveAfterUnmount>
+      1
+    </DynamicModuleLoader>
+  );
 };
 
 export default ProfilePage;
